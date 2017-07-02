cbacbb9a0f40e4f752d0f0d53078526fe688a722
Wong Jianming <1249331134@qq.com>
2017-07-02 14:55:46 +0800

    0702 创建qingjia_YiBan解决方案  后台代码尚未修改

---
+++ qingjia_YiBan/qingjia_YiBan/HomePage/SubPage/form_succeed.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using qingjia_YiBan.HomePage.Class;
+
+namespace qingjia_YiBan.SubPage
+{
+    public partial class form_succeed : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string LV_NUM = Request.QueryString["LV_NUM"].ToString();
+            if (!LeaveList.Delete_leaveList(" ID= " + LV_NUM ))
+            {
+                Response.Redirect("form_notaudited.aspx");
+            }
+        }
+
+    }
+}
